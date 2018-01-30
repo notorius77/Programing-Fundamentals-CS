72687226f29940064aa1674135c9a72391e62e37
Ivelin Simeonov <34216590+notorius77@users.noreply.github.com>
2018-01-30 14:42:56 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15DevisibleByTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 3; i <= 100; i += 3) {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
